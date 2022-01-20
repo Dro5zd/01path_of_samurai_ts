96ba8a3bc049ada2c8c5c0d6f8d0650252da9e1e
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-20 18:41:55 +0600

    Lesson9

---
+++ src/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>Simple HTML</li>
+                <li>JavaScript</li>
+                <li>Css</li>
+                <li>TypeScrypt</li>
+            </ul>
+        </div>)
+}
+
+export default Technologies
