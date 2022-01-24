49e4ec37b9751232fbbfa3124135dcd38989aa9e
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-24 16:33:34 +0600

    Lesson12start

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,7 @@ const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
-            <nav className='nav'>
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-            </nav>
+
             <div className='content'>
                 <div><img
                     src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
