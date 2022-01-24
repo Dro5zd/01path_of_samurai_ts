eb9fb53d34a9e6364bcd3fbcac2c61697255d989
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-24 14:17:18 +0600

    Lesson11start

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return(
-      <div>
-
-      </div>
-  )
+    // @ts-ignore
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://i.pinimg.com/236x/be/62/4f/be624f8809034d83940b6d2346afad92--monogram-logo-logo-google.jpg' alt={"#"}/>
+            </header>
+            <nav className='navbar'>
+                <div><a>Profile</a></div>
+                <div>Messages</div>
+                <div>News</div>
+            </nav>
+            <div className='content'>
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
