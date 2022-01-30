af9adb5c3b8399194e52c407ded27d86590dfc99
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-30 16:23:55 +0600

    add Active class

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css'
 
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}><a href='/profile'>Profile</a></div>
-        <div className={s.item}><a href='/dialogs'>Messages</a></div>
+        <div><NavLink to ='/profile' className={navData => navData.isActive?s.active:s.item}>Profile</NavLink></div>
+        <div><NavLink to ='/dialogs' className={navData => navData.isActive?s.active:s.item}>Messages</NavLink></div>
         <div className={s.item}><a>News</a></div>
         <div className={s.item}><a>Music</a></div>
         <div className={s.item}><a>Settings</a></div>
