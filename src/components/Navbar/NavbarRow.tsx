d5f501d2cb7e1009c176f0ec39a9feab83d076cb
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-31 10:06:03 +0600

    add NavbarRow

---
+++ src/components/Navbar/NavbarRow.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+NavbarRow.propTypes = {
+    
+};
+
+function NavbarRow(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default NavbarRow;
