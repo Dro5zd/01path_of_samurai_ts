dfcd99683650e1cb3f1cbef704fcf3832c88be4c
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-03-17 00:58:23 +0600

    lesson 42 reducers add

---
+++ src/redux/store-redux.tsx
@@ -0,0 +1,11 @@
+import {combineReducers, createStore} from '@reduxjs/toolkit';
+
+let reducers = combineReducers({
+    profilePage: {}
+    dialogsPage:
+})
+
+let store = createStore(reducers)
+
+
+export default store
