d863fb0edb3d39bf34b81b5ebac5690763932867
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-03 18:01:33 +0600

    redusers edit start

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,4 +1,3 @@
-import {addPostAC, updateNewPostAC} from './profile-reducer';
 import {v1} from 'uuid';
 
 
@@ -12,124 +11,133 @@ export type LocationType = {
 }
 
 export type UsersType = {
-    id: number
+    id: string
     avatar: string
     fullName: string
     status : string
     location: LocationType
+    follow: boolean
 }
 
 export type ActionsTypes =
-    ReturnType<typeof addPostAC>
-    | ReturnType<typeof updateNewPostAC>
-    | ReturnType<typeof addMessageAC>
-    | ReturnType<typeof updateNewMessageAC>
+    ReturnType<typeof followAC>
+    | ReturnType<typeof unfollowAC>
 
 
 const initialState: UsersReducerType = {
     users: [
         {
-            id: 1,
+            id: v1(),
             fullName: 'Homer Simpson',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-8.png',
             status: 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield'}
+            location: { country: 'USA', city: 'Springfield'},
+            follow: true
         },
         {
-            id: 2,
+            id: v1(),
             fullName: 'Lisa Simpson',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-1.png',
-            status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            status : 'Pray for Ukraine!',
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 3,
+            id: v1(),
             fullName: 'Bart Simpson',
             avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTal9zpW8P7puFa4bi8cFTU26yK6KgXx1rTA_6BRyY05-j74hs4K1CUc7v70w4RX6tHKns&usqp=CAU',
             status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 4,
+            id: v1(),
             fullName: 'Marge Simpson',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-7.png',
             status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 5,
+            id: v1(),
             fullName: 'Krusty the Clown',
             avatar: 'https://whatsondisneyplus.com/wp-content/uploads/2021/09/krusty.png',
             status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 6,
+            id: v1(),
             fullName: 'Maggie Simpson',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-6.png',
             status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            location: { country: 'USA', city: 'Springfield', },
+            follow: false
         },
         {
-            id: 7,
+            id: v1(),
             fullName: 'Ralph Wiggum',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-4.png',
             status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 8,
+            id: v1(),
             fullName: 'Dr. Hibbert',
             avatar: 'https://www.disneyplusinformer.com/wp-content/uploads/2021/09/The-Simpsons-Profile-Icons-3.png',
-            status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            status : 'Russian warship go fuck yourself',
+            location: { country: 'USA', city: 'Springfield', },
+            follow: false
         },
         {
-            id: 9,
+            id: v1(),
             fullName: 'Ned Flanders',
             avatar: 'https://static.wikia.nocookie.net/simpsons/images/2/2e/Season_23_Icon.jpg',
-            status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            status : 'God safe Ukraine!',
+            location: { country: 'USA', city: 'Springfield', },
+            follow: true
         },
         {
-            id: 10,
+            id: v1(),
             fullName: 'Abraham Simpson II',
             avatar: 'https://static.wikia.nocookie.net/simpsons/images/5/5f/Season_24_Icon.jpg',
-            status : 'I like Springfield!',
-            location: { country: 'USA', city: 'Springfield', }
+            status : 'Stand with Ukraine!',
+            location: { country: 'USA', city: 'Springfield', },
+            follow: false
         },
     ]
 }
 
 const usersReducer = (state = initialState, action: ActionsTypes): UsersReducerType => {
     switch (action.type) {
-        case 'ADD-MESSAGE': {
+        case 'FOLLOW': {
 
+            return {
+                [...state, users: state.users.map(u=>u)]
+            }
+        }
+
+        case 'UNFOLLOW': {
             return {
                 ...state
             }
         }
-
-        case 'UPDATE-NEW-MESSAGE': {
-            return {
-                ...state
-            }
-        }
-
     }
     return state
 }
 
-export const addMessageAC = () => {
+export const followAC = (userId) => {
     return {
-        type: 'ADD-MESSAGE',
+        type: 'FOLLOW',
+        userId
     } as const
 }
 
-export const updateNewMessageAC = (newMessage: string) => {
+export const unfollowAC = (userId) => {
     return {
-        type: 'UPDATE-NEW-MESSAGE',
-        newMessage: newMessage
+        type: 'UNFOLLOW',
+        userId
     } as const
 }
 
